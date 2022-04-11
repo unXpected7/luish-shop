325949f4c7a13448864652d5ba83912c1cd33399
unXpected7 <ilyasyusuf002@gmail.com>
2022-04-11 17:05:44 +0700

    finalalitation myserver

---
+++ myserver/app.js
@@ -0,0 +1,70 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+const cors = require('cors');
+const {decodeToken} = require('./middlewares');
+const productRoute = require('./app/product/router');
+const categoryRoute = require('./app/category/routes');
+const tagRoute = require('./app/tag/router');
+const authRoute = require('./app/auth/router');
+const deliveryAddressRoute = require('./app/deliveryAddress/router');
+const cartRoute = require('./app/cart/router');
+const orderRoute = require('./app/order/router');
+const invoiceRoute = require('./app/invoice/router');
+// var indexRouter = require('./routes/index');
+// var usersRouter = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(cors()); //? agar api dapat diakses oleh react
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(decodeToken());
+
+app.use('/auth', authRoute);
+app.use('/api', productRoute);
+app.use('/api', categoryRoute);
+app.use('/api', tagRoute);
+app.use('/api', deliveryAddressRoute);
+app.use('/api', cartRoute);
+app.use('/api', orderRoute);
+app.use('/api', invoiceRoute);
+
+//? not use multer cuz it's not a file upload
+
+//? HOME
+app.use('/', (req,res) =>{
+  res.render('index',{
+    title: 'eduwork services'
+  });
+});
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {}; //? jika env = development maka error ditampilkan, teteapi jika prodcution maka error tdk ditampilkan
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
+
